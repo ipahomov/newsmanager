e2917947fc119984e50882bf55827152cee0ab54
Igor <p@m.com>
2016-04-10 10:24:12 +0300

    Add users service

--- services/src/main/java/by/it/academy/services/IUserService.java
+++ services/src/main/java/by/it/academy/services/IUserService.java
@@ -1,7 +1,16 @@
 package by.it.academy.services;
 
+import by.it.academy.model.User;
+
 /**
- *
+ * interface for user service
  */
 public interface IUserService {
+
+    /**
+     * Getting user from dao module
+     * @param email of user
+     * @return user
+     */
+    User getUserByEmail(String email);
 }
--- services/src/main/java/by/it/academy/services/UserService.java
+++ services/src/main/java/by/it/academy/services/UserService.java
@@ -1,8 +1,20 @@
 package by.it.academy.services;
 
+import by.it.academy.dao.IUserDao;
+import by.it.academy.dao.UserDao;
+import by.it.academy.model.User;
+
 /**
  *
  */
-public class UserService {
+public class UserService implements IUserService {
+    private IUserDao userDao;
 
+    public UserService(){
+        userDao = UserDao.getInstance();
+    }
+
+    public User getUserByEmail(String email) {
+        return userDao.getUserByEmail(email);
+    }
 }
