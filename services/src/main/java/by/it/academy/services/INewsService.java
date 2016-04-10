7dbab06297ca01e63908ff76f1f0df9ee8bc30e0
ipahomov <ipahomov13@gmail.com>
2016-04-10 11:11:46 +0300

    Add news service

--- services/src/main/java/by/it/academy/services/INewsService.java
+++ services/src/main/java/by/it/academy/services/INewsService.java
@@ -1,7 +1,46 @@
 package by.it.academy.services;
 
+import by.it.academy.model.News;
+
+import java.util.List;
+
 /**
- * Created by Leckter on 09.04.2016.
+ * interface for news service
  */
 public interface INewsService {
+
+    /**
+     * Add service for news
+     * @param news adding to list
+     * @return int value of adding result ( >0 - successfully)
+     */
+    int addNews(News news);
+
+    /**
+     * Delete service for news
+     * @param id of deleting news
+     * @return int value of deleting result ( >0 - successfully)
+     */
+    int deleteNews(int id);
+
+    /**
+     * Edit service for news
+     * @param news editing
+     * @return int value of editing result ( >0 - successfully)
+     */
+    int editNews(News news);
+
+    /**
+     * Get all news
+     * @return List Collection of news
+     */
+    List<News> getAllNews();
+
+    /**
+     * Get all news sorted by one category
+     * @param category of news
+     * @return List Collection of news
+     */
+    List<News> getNewsByCategoryId(String category);
+
 }
--- services/src/main/java/by/it/academy/services/NewsService.java
+++ services/src/main/java/by/it/academy/services/NewsService.java
@@ -1,7 +1,38 @@
 package by.it.academy.services;
 
+import by.it.academy.dao.INewsDao;
+import by.it.academy.dao.NewsDao;
+import by.it.academy.model.News;
+
+import java.util.List;
+
 /**
- * Created by Leckter on 09.04.2016.
+ * Realisation of INewsSerice interface
  */
-public class NewsService {
+public class NewsService implements INewsService {
+    private INewsDao newsDao;
+
+    public NewsService(){
+        newsDao = NewsDao.getInstance();
+    }
+
+    public int addNews(News news) {
+        return newsDao.addNews(news);
+    }
+
+    public int deleteNews(int id) {
+        return newsDao.deleteNews(id);
+    }
+
+    public int editNews(News news) {
+        return newsDao.editNews(news);
+    }
+
+    public List<News> getAllNews() {
+        return newsDao.getAllNews();
+    }
+
+    public List<News> getNewsByCategoryId(String category) {
+        return newsDao.getNewsByCategoryId(category);
+    }
 }
